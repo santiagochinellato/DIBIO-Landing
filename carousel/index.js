49ca5b4ee5ec1d3eea8294e57978d76654188db7
santiago chinellato <santiagochinellato@gmail.com>
2022-11-25 13:12:49 -0300

    cambios en servicios

--- carousel/index.js
+++ carousel/index.js
@@ -1,45 +1,360 @@
-$(".carouselBackground").slick({
+$(".carousel").slick({
   slidesToShow: 1,
   slidesToScroll: 1,
-  arrows: false,
-  // fade: true,
-  asNavFor: ".carouselInfo",
-  speed: 1500,
-  focusOnSelect: false,
-  pauseOnFocus: false,
-  pauseOnHover: true,
-  autoplay: true,
-  autoplaySpeed: 5000,
-  Infinite: false,
-});
-// $(".carouselImagenes").slick({
-//   slidesToShow: 1,
-//   slidesToScroll: 1,
-//   arrows: false,
-//   // fade: true,
-//   asNavFor: ".carouselInfo",
-//   speed: 1900,
-//   pauseOnFocus: false,
-//   pauseOnHover: false,
-//   // autoplay: true,
-//   // autoplaySpeed: 5000,
-//   Infinite: true,
-// });
-$(".carouselInfo").slick({
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  asNavFor: ".carouselBackground",
+  // asNavFor: ".carouselBackground",
   arrows: true,
   dots: false,
   fade: true,
   // centerMode: true,
   focusOnSelect: false,
-  speed: 2500,
+  speed: 1000,
   pauseOnFocus: false,
   pauseOnHover: true,
   autoplay: true,
-  autoplaySpeed: 5000,
+  autoplaySpeed: 3000,
   Infinite: false,
   prevArrow: $(".prev"),
   nextArrow: $(".next"),
+});
+
+// servicios
+
+// box1
+
+$(function () {
+  $(".box1").hover(
+    function () {
+      $(".box2 > .contenedorTexto > .TituloEIcono >.textoServicios").css(
+        "display",
+        "none"
+      );
+    },
+    function () {
+      // vuelve a dejar el <div> como estaba al hacer el "mouseout"
+      $(".box2 > .contenedorTexto > .TituloEIcono >.textoServicios").css(
+        "display",
+        "inline"
+      );
+    }
+  );
+
+  $(".box1").hover(
+    function () {
+      $(".box3 > .contenedorTexto > .TituloEIcono >.textoServicios").css(
+        "display",
+        "none"
+      );
+    },
+    function () {
+      // vuelve a dejar el <div> como estaba al hacer el "mouseout"
+      $(".box3 > .contenedorTexto > .TituloEIcono >.textoServicios").css(
+        "display",
+        "inline"
+      );
+    }
+  );
+
+  $(".box1").hover(
+    function () {
+      $(".box4 > .contenedorTexto > .TituloEIcono >.textoServicios").css(
+        "display",
+        "none"
+      );
+    },
+    function () {
+      // vuelve a dejar el <div> como estaba al hacer el "mouseout"
+      $(".box4 > .contenedorTexto > .TituloEIcono >.textoServicios").css(
+        "display",
+        "inline"
+      );
+    }
+  );
+
+  $(".box1").hover(
+    function () {
+      $(".box5 > .contenedorTexto > .TituloEIcono >.textoServicios").css(
+        "display",
+        "none"
+      );
+    },
+    function () {
+      // vuelve a dejar el <div> como estaba al hacer el "mouseout"
+      $(".box5 > .contenedorTexto > .TituloEIcono >.textoServicios").css(
+        "display",
+        "inline"
+      );
+    }
+  );
+});
+
+// box2
+
+$(function () {
+  $(".box2").hover(
+    function () {
+      $(".box1 > .contenedorTexto > .TituloEIcono >.textoServicios").css(
+        "display",
+        "none"
+      );
+    },
+    function () {
+      // vuelve a dejar el <div> como estaba al hacer el "mouseout"
+      $(".box1 > .contenedorTexto > .TituloEIcono >.textoServicios").css(
+        "display",
+        "inline"
+      );
+    }
+  );
+
+  $(".box2").hover(
+    function () {
+      $(".box3 > .contenedorTexto > .TituloEIcono >.textoServicios").css(
+        "display",
+        "none"
+      );
+    },
+    function () {
+      // vuelve a dejar el <div> como estaba al hacer el "mouseout"
+      $(".box3 > .contenedorTexto > .TituloEIcono >.textoServicios").css(
+        "display",
+        "inline"
+      );
+    }
+  );
+
+  $(".box2").hover(
+    function () {
+      $(".box4 > .contenedorTexto > .TituloEIcono >.textoServicios").css(
+        "display",
+        "none"
+      );
+    },
+    function () {
+      // vuelve a dejar el <div> como estaba al hacer el "mouseout"
+      $(".box4 > .contenedorTexto > .TituloEIcono >.textoServicios").css(
+        "display",
+        "inline"
+      );
+    }
+  );
+
+  $(".box2").hover(
+    function () {
+      $(".box5 > .contenedorTexto > .TituloEIcono >.textoServicios").css(
+        "display",
+        "none"
+      );
+    },
+    function () {
+      // vuelve a dejar el <div> como estaba al hacer el "mouseout"
+      $(".box5 > .contenedorTexto > .TituloEIcono >.textoServicios").css(
+        "display",
+        "inline"
+      );
+    }
+  );
+});
+
+// box3
+
+$(function () {
+  $(".box3").hover(
+    function () {
+      $(".box1 > .contenedorTexto > .TituloEIcono >.textoServicios").css(
+        "display",
+        "none"
+      );
+    },
+    function () {
+      // vuelve a dejar el <div> como estaba al hacer el "mouseout"
+      $(".box1 > .contenedorTexto > .TituloEIcono >.textoServicios").css(
+        "display",
+        "inline"
+      );
+    }
+  );
+
+  $(".box3").hover(
+    function () {
+      $(".box2 > .contenedorTexto > .TituloEIcono >.textoServicios").css(
+        "display",
+        "none"
+      );
+    },
+    function () {
+      // vuelve a dejar el <div> como estaba al hacer el "mouseout"
+      $(".box2 > .contenedorTexto > .TituloEIcono >.textoServicios").css(
+        "display",
+        "inline"
+      );
+    }
+  );
+
+  $(".box3").hover(
+    function () {
+      $(".box4 > .contenedorTexto > .TituloEIcono >.textoServicios").css(
+        "display",
+        "none"
+      );
+    },
+    function () {
+      // vuelve a dejar el <div> como estaba al hacer el "mouseout"
+      $(".box4 > .contenedorTexto > .TituloEIcono >.textoServicios").css(
+        "display",
+        "inline"
+      );
+    }
+  );
+
+  $(".box3").hover(
+    function () {
+      $(".box5 > .contenedorTexto > .TituloEIcono >.textoServicios").css(
+        "display",
+        "none"
+      );
+    },
+    function () {
+      // vuelve a dejar el <div> como estaba al hacer el "mouseout"
+      $(".box5 > .contenedorTexto > .TituloEIcono >.textoServicios").css(
+        "display",
+        "inline"
+      );
+    }
+  );
+});
+
+// box 4
+
+$(function () {
+  $(".box4").hover(
+    function () {
+      $(".box2 > .contenedorTexto > .TituloEIcono >.textoServicios").css(
+        "display",
+        "none"
+      );
+    },
+    function () {
+      // vuelve a dejar el <div> como estaba al hacer el "mouseout"
+      $(".box2 > .contenedorTexto > .TituloEIcono >.textoServicios").css(
+        "display",
+        "inline"
+      );
+    }
+  );
+
+  $(".box4").hover(
+    function () {
+      $(".box3 > .contenedorTexto > .TituloEIcono >.textoServicios").css(
+        "display",
+        "none"
+      );
+    },
+    function () {
+      // vuelve a dejar el <div> como estaba al hacer el "mouseout"
+      $(".box3 > .contenedorTexto > .TituloEIcono >.textoServicios").css(
+        "display",
+        "inline"
+      );
+    }
+  );
+
+  $(".box4").hover(
+    function () {
+      $(".box1 > .contenedorTexto > .TituloEIcono >.textoServicios").css(
+        "display",
+        "none"
+      );
+    },
+    function () {
+      // vuelve a dejar el <div> como estaba al hacer el "mouseout"
+      $(".box1 > .contenedorTexto > .TituloEIcono >.textoServicios").css(
+        "display",
+        "inline"
+      );
+    }
+  );
+
+  $(".box4").hover(
+    function () {
+      $(".box5 > .contenedorTexto > .TituloEIcono >.textoServicios").css(
+        "display",
+        "none"
+      );
+    },
+    function () {
+      // vuelve a dejar el <div> como estaba al hacer el "mouseout"
+      $(".box5 > .contenedorTexto > .TituloEIcono >.textoServicios").css(
+        "display",
+        "inline"
+      );
+    }
+  );
+});
+
+// box5
+
+$(function () {
+  $(".box5").hover(
+    function () {
+      $(".box2 > .contenedorTexto > .TituloEIcono >.textoServicios").css(
+        "display",
+        "none"
+      );
+    },
+    function () {
+      // vuelve a dejar el <div> como estaba al hacer el "mouseout"
+      $(".box2 > .contenedorTexto > .TituloEIcono >.textoServicios").css(
+        "display",
+        "inline"
+      );
+    }
+  );
+
+  $(".box5").hover(
+    function () {
+      $(".box3 > .contenedorTexto > .TituloEIcono >.textoServicios").css(
+        "display",
+        "none"
+      );
+    },
+    function () {
+      // vuelve a dejar el <div> como estaba al hacer el "mouseout"
+      $(".box3 > .contenedorTexto > .TituloEIcono >.textoServicios").css(
+        "display",
+        "inline"
+      );
+    }
+  );
+
+  $(".box5").hover(
+    function () {
+      $(".box4 > .contenedorTexto > .TituloEIcono >.textoServicios").css(
+        "display",
+        "none"
+      );
+    },
+    function () {
+      // vuelve a dejar el <div> como estaba al hacer el "mouseout"
+      $(".box4 > .contenedorTexto > .TituloEIcono >.textoServicios").css(
+        "display",
+        "inline"
+      );
+    }
+  );
+
+  $(".box4").hover(
+    function () {
+      $(".box1 > .contenedorTexto > .TituloEIcono >.textoServicios").css(
+        "display",
+        "none"
+      );
+    },
+    function () {
+      // vuelve a dejar el <div> como estaba al hacer el "mouseout"
+      $(".box1 > .contenedorTexto > .TituloEIcono >.textoServicios").css(
+        "display",
+        "inline"
+      );
+    }
+  );
 });
